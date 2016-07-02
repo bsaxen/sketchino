0609ce14fd6a7875f1384377c43b87067af01cd6
bsaxen <benny.saxen@gmail.com>
2016-07-02 20:58:13 +0200

    Update arduino-sniffer-test.ino

--- arduino-sniffer-test.ino
+++ arduino-sniffer-test.ino
@@ -1,1 +1,30 @@
+//=====================
+// arduino-sniffer-test.ino
+// 2016-07-02
+//=====================
 
+void onoff(int pin,int ll)
+{
+ int i;
+ for(i=0;i<ll;i++)
+ {
+  digitalWrite(pin,HIGH);
+  delay(100);
+  digitalWrite(pin,LOW);
+  //delay(1);  
+ }
+}
+
+void setup() 
+{
+ int i;
+ for(i=2;i<13;i++)pinMode(i,OUTPUT);
+}
+
+void loop() 
+{
+int i;
+onoff(5,1);
+onoff(6,1);
+
+}
